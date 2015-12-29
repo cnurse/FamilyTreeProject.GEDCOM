cf0494b78707d473589ca261763df11618278a94
Charles Nurse <charlesnurse@gmail.com>
2015-12-29 12:18:14 -0800

    Updated to version 1.1 and restored Nuget based dependencies

--- tests/FamilyTreeProject.GEDCOM.Tests/AssemblyInfo.cs
+++ tests/FamilyTreeProject.GEDCOM.Tests/AssemblyInfo.cs
@@ -30,15 +30,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("07bba627-1a70-4488-bd1f-662e0a8ba48a")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
